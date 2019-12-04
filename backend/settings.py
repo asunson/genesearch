3e000ec1c866ec244e6b92e9a7f6a420a54feab3
asunson <aaronsunson@gmail.com>
2019-12-03 23:48:20 -0500

    more fixes

--- backend/settings.py
+++ backend/settings.py
@@ -134,7 +134,8 @@ STATIC_URL = '/static/'
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
     'http://localhost:8000',
-    'http://127.0.0.1:8000'
+    'http://127.0.0.1:8000',
+    'https://awsun-genesearch.herokuapp.com'
 ]
 
 # Activate Django-Heroku.
